2a3871d4cb25b6beb7e72ff87a4da166d2ec1431
BalachandarRJ <balachandar61096@gmail.com>
2024-12-18 11:09:50 +0530

    done

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author balu
+ *
+ */
+module JavaGitHubProject {
+}
---
+++ src/org/git/JavaGitHub1.java
@@ -0,0 +1,7 @@
+package org.git;
+
+public class JavaGitHub1 {
+public static void main(String[] args) {
+	System.out.println("I am created my new project");
+}
+}
